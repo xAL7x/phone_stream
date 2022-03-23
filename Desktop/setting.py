1f90ca274d8d922d61c4c4ac70a038e75a6f3eab
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2022-03-23 17:53:04 +0300

    إصلاح وتنظيف أكواد
    - تجربة الرفع والمشاركة
    - إضافة مسافات قبل وبعد علامات اليساوي للتوافق مع معايير  [PEP 8](https://peps.python.org/pep-0008/#variable-annotations)
    - تعديل الindentation
    - تعديل الreadme

--- Desktop/setting.py
+++ Desktop/setting.py
@@ -1,45 +1,55 @@
 import socket
-server=socket.socket()
-host="IP";port=8080
-server.bind((host,port));server.listen()
-client,addr=server.accept()
-#___
-### SERVER Files
-file='main.py'
-text=open(file,'r');text_read=text.read()
-text_write=open(file,'w')
-Target_name_file='target.txt'
-### ______________________________________________________
 
+server = socket.socket()
+host = "IP"
+port=8080
+server.bind((host,port))
+server.listen()
+client,addr=server.accept()
+
+### SERVER Files
+file = 'main.py'
+text = open(file,'r')
+text_read = text.read()
+text_write = open(file,'w')
+Target_name_file = 'target.txt'
 
 while True: 
-    ###_______________________________________________________
-        Y=input('\n What do you want to add [from 1 to ... (exit)]: ')
-        if Y=='exit':
-            exit()
-        client.send(f'{Y}'.encode())
-        image_name=input('Image name: ')
-        target=input('Target: ')
+    Y = input('\n What do you want to add [from 1 to ... (exit)]:')
+    if Y == 'exit':
+        exit()
+    
+    client.send(f'{Y}'.encode())
+    image_name = input('Image name: ')
+    target = input('Target: ')
 
-        file_read=open(f'{Y}{Target_name_file}','r');file_read=file_read.read()
-        ### SERVER
-        if 'http' in target:
-                textY=text_read.replace(f'{file_read}',f'open("{target}")')
-                textY=text_read.replace(f'''#Y{Y} URL OR APP
-        pass''',f'open("{target}")')
-                text_write.write(f"{textY}");text_write.close()
-                #### Make files for editing
-                file=open(f'{Y}{Target_name_file}','w');file.write(f'open("{target}")');file.close()
-        else:
-                textY=text_read.replace(f'''#Y{Y} URL OR APP
-        pass''',f'subprocess.call("{target}")')
-                textY=text_read.replace(f'{file_read}',f'subprocess.call("{target}")')
-                text_write.write(textY);text_write.close()
-                #### MAke files for editing
-                file=open(f'{Y}{Target_name_file}','w');file.write(f'subprocess.call("{target}")');file.close()
-        ### SERVER END ###
-            #________________________________________________________________________________________________________
-        file = open(f'{image_name}', 'rb')
-        image_data = file.read(819200)
-        client.send(image_data)
+    file_read = open(f'{Y}{Target_name_file}','r')
+    file_read = file_read.read()
+    
+    ### SERVER
+    if 'http' in target:
+        textY = text_read.replace(f'{file_read}',f'open("{target}")')
+        textY = text_read.replace(f'''#Y{Y} URL OR APP pass''',f'open("{target}")')
+        text_write.write(f"{textY}")
+        text_write.close()
+        
+        #### Make files for editing
+        file = open(f'{Y}{Target_name_file}','w')
+        file.write(f'open("{target}")')
         file.close()
+    else:
+        textY = text_read.replace(f'''#Y{Y} URL OR APP pass''',f'subprocess.call("{target}")')
+        textY = text_read.replace(f'{file_read}',f'subprocess.call("{target}")')
+        text_write.write(textY)
+        text_write.close()
+        
+        #### MAke files for editing
+        file = open(f'{Y}{Target_name_file}','w')
+        file.write(f'subprocess.call("{target}")')
+        file.close()
+### SERVER END ###
+
+    file = open(f'{image_name}', 'rb')
+    image_data = file.read(819200)
+    client.send(image_data)
+    file.close()
--- phone/main.py
+++ phone/main.py
@@ -1,84 +1,84 @@
-from tkinter import * 
-#
-from tkinter.ttk import *
-#SOCKET
 import socket
+
+from tkinter import * 
+from tkinter.ttk import *
+
 client=socket.socket()
 client.connect(('IP',8080))    
 
-if True:
-#	# creating tkinter window
-	root = Tk()  
-	root.configure(bg='#000')
-#	  
-#	# Creating a photoimage object to use image 
-#	
-	Y1_image=PhotoImage(file="1image.png")        
-	Y2_image=PhotoImage(file="2image.png")
-	Y3_image=PhotoImage(file="3image.png")
-	Y4_image=PhotoImage(file="4image.png")
-	Y5_image=PhotoImage(file='5image.png')
-	Y6_image=PhotoImage(file='6image.png')
-	Y7_image=PhotoImage(file='7image.png')
-	Y8_image=PhotoImage(file='8image.png')
-	Y9_image=PhotoImage(file='9image.png')
-	Y10_image=PhotoImage(file='10image.png')
-	Y11_image=PhotoImage(file='11image.png')
-	Y12_image=PhotoImage(file='12image.png')
-	def Y1_command():
-            client.send('1'.encode())
-           
-	def Y2_command():
-            client.send('2'.encode())
-            
-	def Y3_command():
-            client.send('3'.encode()) 
-            
-	def Y4_command():
-            client.send('4'.encode())	
-                
-	def Y5_command():
-            client.send('5'.encode())
+# creating tkinter window
+root = Tk()  
+root.configure(bg='#000')
+
+# Creating a photoimage object to use image 
+Y1_image = PhotoImage(file="1image.png")        
+Y2_image = PhotoImage(file="2image.png")
+Y3_image = PhotoImage(file="3image.png")
+Y4_image = PhotoImage(file="4image.png")
+Y5_image = PhotoImage(file='5image.png')
+Y6_image = PhotoImage(file='6image.png')
+Y7_image = PhotoImage(file='7image.png')
+Y8_image = PhotoImage(file='8image.png')
+Y9_image = PhotoImage(file='9image.png')
+Y10_image = PhotoImage(file='10image.png')
+Y11_image = PhotoImage(file='11image.png')
+Y12_image = PhotoImage(file='12image.png')
+
+def Y1_command():
+    client.send('1'.encode())
         
-	def Y6_command():
-            client.send('6'.encode())
-            
-	def Y7_command():
-            client.send('7'.encode())
+def Y2_command():
+    client.send('2'.encode())
         
-	def Y8_command():
-            client.send('8'.encode())
-	def Y9_command():
-            client.send('9'.encode())
-            
-	def Y10_command():
-            client.send('10'.encode())
+def Y3_command():
+    client.send('3'.encode()) 
         
-	def Y11_command():
-            client.send('11'.encode())
-	def Y12_command():
-            client.send('12'.encode())
-	Y1_Button=Button(root,image=Y1_image,command=Y1_command).place(y=0,x=720)
-
-	Y2_Button=Button(root,image = Y2_image,command=Y2_command).place(y=370,x=720)
-
-	Y3_Button=Button(root,image = Y3_image,command=Y3_command).place(y=740,x=720)
-
-	Y4_Button=Button(root,image = Y4_image,command=Y4_command).place(y=1110,x=720)
-
-	Y5_Button=Button(root,image = Y5_image,command=Y5_command).place(y=1480,x=720)
-
-	Y6_Button=Button(root,image = Y6_image,command=Y6_command).place(y=1850,x=720)
-
-	Y7_Button=Button(root,image = Y7_image,command=Y7_command).place(y=0,x=360)
-
-	Y8_Button=Button(root,image = Y8_image,command=Y8_command).place(y=370,x=360)
+def Y4_command():
+    client.send('4'.encode())	
+            
+def Y5_command():
+    client.send('5'.encode())
     
-	Y9_Button=Button(root,image = Y9_image,command=Y9_command).place(y=740,x=360)
+def Y6_command():
+    client.send('6'.encode())
+        
+def Y7_command():
+    client.send('7'.encode())
     
-	Y10_Button=Button(root,image = Y10_image,command=Y10_command).place(y=1110,x=360)
+def Y8_command():
+    client.send('8'.encode())
+def Y9_command():
+    client.send('9'.encode())
+        
+def Y10_command():
+    client.send('10'.encode())
+    
+def Y11_command():
+    client.send('11'.encode())
+def Y12_command():
+    client.send('12'.encode())
 
-	Y11_Button=Button(root,image = Y11_image,command=Y11_command).place(y=1480,x=360)
+Y1_Button = Button(root,image=Y1_image,command=Y1_command).place(y=0,x=720)
 
-	Y12_Button=Button(root,image = Y12_image,command=Y12_command).place(y=1850,x=360)
-	root.mainloop()
+Y2_Button = Button(root,image = Y2_image,command=Y2_command).place(y=370,x=720)
+
+Y3_Button = Button(root,image = Y3_image,command=Y3_command).place(y=740,x=720)
+
+Y4_Button = Button(root,image = Y4_image,command=Y4_command).place(y=1110,x=720)
+
+Y5_Button = Button(root,image = Y5_image,command=Y5_command).place(y=1480,x=720)
+
+Y6_Button = Button(root,image = Y6_image,command=Y6_command).place(y=1850,x=720)
+
+Y7_Button = Button(root,image = Y7_image,command=Y7_command).place(y=0,x=360)
+
+Y8_Button = Button(root,image = Y8_image,command=Y8_command).place(y=370,x=360)
+
+Y9_Button = Button(root,image = Y9_image,command=Y9_command).place(y=740,x=360)
+
+Y10_Button = Button(root,image = Y10_image,command=Y10_command).place(y=1110,x=360)
+
+Y11_Button = Button(root,image = Y11_image,command=Y11_command).place(y=1480,x=360)
+
+Y12_Button = Button(root,image = Y12_image,command=Y12_command).place(y=1850,x=360)
+root.mainloop()
